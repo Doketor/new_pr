eb0c9d60ab6a8a83b93557beabc0f19fd2332d91
Pavel Zharkikh <doketor@gmail.com>
2022-04-08 10:28:11 +0300

    kljl

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,16 @@
+from dataclasses import field
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Bin
+
+# admin.site.register(Product, ProductAdmin)
+admin.site.register(Bin)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'id', 'cost']
+    fields = ['id', 'title']
+    list_filter = ['cost']
+    search_fields = ['title']
+
+# admin.site.register(Product, ProductAdmin)
--- service/models.py
+++ service/models.py
@@ -5,7 +5,6 @@ class Product(models.Model):
     id = models.IntegerField(primary_key=True)
     cost = models.CharField(max_length=200, null=True)
     title = models.CharField(max_length=200, null=True)
-
     def __str__(self):
         return self.title
         
@@ -13,3 +12,5 @@ class Bin(models.Model):
     id = models.IntegerField(primary_key=True)
     cost = models.CharField(max_length=200, null=True)
     title = models.CharField(max_length=200, null=True)
+    def __str__(self):
+        return self.title
